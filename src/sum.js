31c027efc68d7ef26fd8f82fc36eba66e7124e2e
St80ene <etienejames5@gmail.com>
2024-11-16 14:33:39 +0100

    Formatting
    Signed-off-by: St80ene <etienejames5@gmail.com>

--- src/sum.js
+++ src/sum.js
@@ -1,12 +1,5 @@
 function sum(a, b) {
-  // createEmitAndSemantic
-  // DiagnosticsBuilder
-  // Programewvyuvcv
   return a + b;
 }
-
-// function getName() {
-//   return '';
-// }
 
 module.exports = sum;
